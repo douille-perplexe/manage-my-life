0ebd515aae8003398f501fd5a6059cc4a3c67f5e
MORLAT Sacha <sachamorlat@gmail.com>
2025-03-22 16:16:38 +0100

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,15 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: "class",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: {
-          DEFAULT: '#3b82f6',
-          dark: '#2563eb',
-        },
-        secondary: {
-          DEFAULT: '#10b981',
-          dark: '#059669',
-        },
         white: "#ffffff",
         gray: {
           100: "#f3f4f6",
@@ -25,12 +17,12 @@ const config: Config = {
           300: "#d1d5db",
           500: "#6b7280",
           700: "#374151",
-          800: "#1f2937"
+          800: "#1f2937",
         },
         blue: {
           200: "#93c5fd",
           400: "#60a5fa",
-          500: "#3b82f6"
+          500: "#3b82f6",
         },
         "dark-bg": "#101214",
         "dark-secondary": "#1d1f21",
@@ -38,9 +30,13 @@ const config: Config = {
         "blue-primary": "#0275ff",
         "stroke-dark": "#2d3135",
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [],
-}
-
-export default config
+};
+export default config;
