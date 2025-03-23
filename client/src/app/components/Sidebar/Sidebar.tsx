499d6f25c79b8ce5a49089a9802bd8f239df23a2
Sacha MORLAT <sachamorlat@gmail.com>
2025-03-23 16:20:55 +0100

    move files in client directory

---
+++ client/src/app/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,143 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import {
+  AlertCircle,
+  AlertOctagon,
+  AlertTriangle,
+  Briefcase,
+  ChevronDown,
+  ChevronUp,
+  Home,
+  Layers3,
+  LockIcon,
+  LucideIcon,
+  Search,
+  Settings,
+  ShieldAlert,
+  User,
+  Users,
+  X,
+} from "lucide-react";
+import SidebarLink from "@/app/components/Sidebar/SidebarLink";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state/state";
+
+const Sidebar = () => {
+  const [showProjects, setShowProjects] = useState(true);
+  const [showPriority, setShowPriority] = useState(true);
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
+  const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between 
+  shadow-xl transition-all 
+  duration-300 h-full z-40 overflow-y-auto bg-white ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}`;
+
+  return (
+    <div className={sidebarClassNames}>
+      <div className={"flex h-[100%] w-full flex-col justify-start"}>
+        {/* TOP LOGO */}
+        <div className="dark:bg-dark-bg z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3">
+          <div className={"text-xl font-bold text-gray-800"}>MML</div>
+          {!isSidebarCollapsed && (
+            <button
+              className={"py-3"}
+              onClick={() => {
+                dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+              }}
+            >
+              <X className="h-6 w-6 text-gray-800 hover:text-gray-500" />
+            </button>
+          )}
+        </div>
+        {/* TEAM */}
+        <div
+          className={
+            "flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700"
+          }
+        >
+          <Image src={"/MML.png"} alt={"Logo"} width={40} height={40} />
+          <div>
+            <h3 className={"text-md font-bold tracking-widest"}>MML TEAM</h3>
+            <div className={"mt-1 flex items-start gap-2"}>
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
+          </div>
+        </div>
+        {/* NAVBAR LINKS */}
+        <nav className={"z-10 w-full"}>
+          <SidebarLink href={"/"} icon={Home} label={"Home"} />
+          <SidebarLink href={"/timeline"} icon={Briefcase} label={"Timeline"} />
+          <SidebarLink href={"/search"} icon={Search} label={"Search"} />
+          <SidebarLink href={"/settings"} icon={Settings} label={"Settings"} />
+          <SidebarLink href={"/users"} icon={User} label={"Users"} />
+          <SidebarLink href={"/teams"} icon={Users} label={"Teams"} />
+        </nav>
+
+        <button
+          onClick={() => setShowProjects((prev) => !prev)}
+          className={
+            "flex w-full items-center justify-between px-8 py-3 text-gray-500"
+          }
+        >
+          <span>Projects</span>
+          {showProjects ? (
+            <ChevronUp className={"h-5 w-5"} />
+          ) : (
+            <ChevronDown className={"h-5 w-5"} />
+          )}
+        </button>
+
+        <button
+          onClick={() => setShowPriority((prev) => !prev)}
+          className={
+            "flex w-full items-center justify-between px-8 py-3 text-gray-500"
+          }
+        >
+          <span>Priority</span>
+          {showPriority ? (
+            <ChevronUp className={"h-5 w-5"} />
+          ) : (
+            <ChevronDown className={"h-5 w-5"} />
+          )}
+        </button>
+
+        {showPriority && (
+          <>
+            <SidebarLink
+              href={"/priority/urgent"}
+              icon={AlertCircle}
+              label={"Urgent"}
+            />
+            <SidebarLink
+              href={"/priority/High"}
+              icon={ShieldAlert}
+              label={"High"}
+            />
+            <SidebarLink
+              href={"/priority/medium"}
+              icon={AlertTriangle}
+              label={"Medium"}
+            />
+            <SidebarLink
+              href={"/priority/low"}
+              icon={AlertOctagon}
+              label={"Low"}
+            />
+            <SidebarLink
+              href={"/priority/backlog"}
+              icon={Layers3}
+              label={"Backlog"}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
