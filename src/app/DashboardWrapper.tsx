aef89efecd15d35a76175ba746a773cc27eb630c
Sacha MORLAT <sachamorlat@gmail.com>
2025-03-23 14:10:24 +0100

    try to fix dark mode

--- src/app/DashboardWrapper.tsx
+++ src/app/DashboardWrapper.tsx
@@ -1,12 +1,14 @@
-"use client"
+"use client";
 
 import React, { useEffect } from "react";
 import Navbar from "@/app/components/Navbar";
 import Sidebar from "@/app/components/Sidebar";
 import StoreProvider, { useAppSelector } from "@/app/redux";
 
-const DashboardLayout = ({children}: {children: React.ReactNode}) => {
-  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
   useEffect(() => {
@@ -15,28 +17,29 @@ const DashboardLayout = ({children}: {children: React.ReactNode}) => {
     } else {
       document.documentElement.classList.remove("dark");
     }
-  })
+  }, [isDarkMode]);
 
   return (
-    <div className="flex min-h-screen w-full bg-white text-gray-900">
-      {/* Sidebar */}
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
-      <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 ${isSidebarCollapsed ? "" : "md:pl-64"}`}>
-        {/* Navbar */}
+      <main
+        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 ${
+          isSidebarCollapsed ? "" : "md:pl-64"
+        }`}
+      >
         <Navbar />
         {children}
       </main>
     </div>
-  )
-}
+  );
+};
 
-
-const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-       <DashboardLayout>{children}</DashboardLayout>
+      <DashboardLayout>{children}</DashboardLayout>
     </StoreProvider>
-  )
-}
+  );
+};
 
 export default DashboardWrapper;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import DashboardWrapper from "@/app/DashboardWrapper";
+import React from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <DashboardWrapper>
-          {children}
-        </DashboardWrapper>
+        <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
   );
