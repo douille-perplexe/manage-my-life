a11a37e2abdd65218254168b7c14a124b431743d
Sacha MORLAT <sachamorlat@gmail.com>
2025-03-24 23:23:20 +0100

    add types

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,5 +1,65 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+export interface Project {
+  id: number;
+  name: string;
+  description?: string;
+  startDate?: string;
+  endDate?: string;
+}
+
+export interface User {
+  userId?: number;
+  username: string;
+  email: string;
+  profilePictureUrl?: string;
+  cognitoId?: number;
+  teamId?: number;
+}
+
+export interface Team {}
+
+export interface Attachment {
+  id: number;
+  fileUrl: string;
+  fileName: string;
+  taskId: number;
+  uploadedById: number;
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  startDate?: string;
+  dueDate?: string;
+  status?: Status;
+  tags?: string[];
+  projectId: number;
+  authorUserId?: number;
+  assigneeUserId?: number;
+  priority?: Priority;
+  author?: User;
+  assignee?: User;
+  comments?: Comment[];
+  attachments?: Attachment[];
+}
+
+export enum Status {
+  ToDo = "To Do",
+  WorkInProgress = "Work In Progress",
+  UnderReview = "Under Review",
+  Completed = "Completed",
+}
+
+export enum Priority {
+  Urgent = "Urgent",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+  Backlog = "Backlog",
+}
+
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
